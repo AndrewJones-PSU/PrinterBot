0a856611badf0f0116c7a3d85426f2b852cc41e5
AndrewJones-PSU <aej5218@psu.edu>
2022-09-01 03:05:10 -0400

    finished moving away from commando

---
+++ deploy-commands.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const { Routes } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+require('dotenv').config();
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON());
+}
+
+
+const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
+
+rest.put(Routes.applicationGuildCommands(process.env.BOT_ID, process.env.DEV_GUILD_ID), { body: commands })
+    .then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+    .catch(console.error);
