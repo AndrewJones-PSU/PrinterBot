f9f6e00770d6c980a663d881994d3cc264a0fba6
AndrewJones-PSU <aej5218@psu.edu>
2022-10-12 01:01:06 -0400

    implemented print functionality

--- printfuncs.js
+++ printfuncs.js
@@ -1,11 +1,30 @@
 // This file holds all functions related to printing and processing files for printing.
 const md = require("markdown-it")();
 const sharp = require("sharp");
+const linkedlist = require("@stdlib/utils-linked-list");
+const ReadWriteLock = require("rwlock");
+const escpos = require("escpos");
+const SerialPort = require("escpos-serialport");
+
+// init event emitter for print loop
+var eventEmitter = new events.EventEmitter();
+
+// printer config
+const device = new SerialPort("COM8");
+const options = {
+    encoding: "GB18030",
+};
+const printer = new escpos.Printer(device, options);
 
 // Load in config.js
 const config = require("./config.js");
 
+// array of all file types to print
 const allowedExtensions = ["png", "jpg", "jpeg", "txt", "md"];
+
+// init queue LL
+var queue = linkedlist();
+var queuelock = new ReadWriteLock();
 
 // This function takes an array of files, and converts them to printable images
 function filesToPrintableImages(files, puppeteerInstance) {
@@ -128,14 +147,93 @@ function validatePrintableImage(imgFile) {
 
 // This function queues an array of image files for printing
 // Images are first validated before being queued
+// After the images are queued, a cut command is queued as well
 // A Mutex is used to prevent multiple threads from queueing images at the same time (this can lead to printing images
 // out of order)
-function queueImages(imgFiles) {}
+function queueImages(imgFiles) {
+    // check that the images are printable
+    for (var i = 0; i < imgFiles.length; i++) {
+        if (!validatePrintableImage(imgFiles[i])) return false;
+    }
+    // add images to queue
+    queuelock.writeLock((release) => {
+        for (var i = 0; i < imgFiles.length; i++) {
+            queue.push(imgFiles[i]);
+        }
+        // after all images, add cut command and release lock
+        queue.push("cut");
+        release();
+        return true;
+    });
+}
 
 // This function prints images in the queue
+// ! This function loops forever, only call it once to start the loop
 // The queue is checked every 1000ms, and if there are images in the queue, they are printed
 // This function does not print if images are being added to the queue (Mutex)
-function loopingPrintQueue() {}
+function loopingPrintQueue() {
+    // check if the queue is empty
+    queuelock.readLock((release) => {
+        if (queue.length > 0) {
+            // if not, init the print loop
+            eventEmitter.emit("printNextImage", release);
+            // the print loop will call this function again when it is done printing
+            // the print loop will also release the read lock when it is done
+        } else {
+            // if there's nothing to print, call this function again in 1000ms
+            release();
+            setTimeout(loopingPrintQueue, 1000);
+        }
+    });
+}
+
+// This event prints the next image in the queue, and calls itself again via an event emitter if there are more images in the queue
+// This function does not check if the image is printable, so it should only be called after the image has been validated
+// This function is probably inefficient since we init a new printer instance every time, and could probably be improved
+eventEmitter.on("printNextImage", (release) => {
+    // load in the image
+    const img = queue.shift();
+    // if the image is undefined, queue is empty, call looping print queue, exit loop
+    if (img === undefined) {
+        release();
+        setTimeout(loopingPrintQueue, 1000);
+        return;
+    }
+    // if the image is "cut", cut the paper, and recall this function
+    if (img === "cut") {
+        device.open((error) => {
+            if (error) {
+                // log errors, recall looping print queue
+                console.log(error);
+                setTimeout(loopingPrintQueue, 1000);
+                return;
+            }
+            printer.cut();
+            printer.close();
+            eventEmitter.emit("printNextImage", release);
+        });
+    }
+    // load the image in, convert to base64
+    const b64image = new Buffer.from(img).toString("base64");
+    // add dataURI header
+    const dataURI = `data:image/png;base64,${b64image}`;
+    // load image and device for printing
+    escpos.Image.load(dataURI, (image) => {
+        device.open((error) => {
+            if (error) {
+                // log errors, recall looping print queue
+                console.log(error);
+                setTimeout(loopingPrintQueue, 1000);
+                return;
+            }
+            // print image, close device, call print loop
+            printer.align("ct");
+            printer.raster(image);
+            printer.close();
+            eventEmitter.emit("printNextImage", release);
+        });
+    });
+});
 
 // export functions
 module.exports = {
